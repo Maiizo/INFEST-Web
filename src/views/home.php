0346985cb65548b209ecaa9406be7e0934bc3f5e
felicia-sw <felicia.sword@gmail.com>
2025-05-22 20:30:54 +0700

    new again

--- src/views/home.php
+++ src/views/home.php
@@ -8,8 +8,23 @@
     
 </head>
 <body>
-    <?php
-    echo "<h1>hi</h1>";
-    ?>
+    <section class="hero" id="home">
+        <div class="hero-content">
+            <p class="hero-subtitle">Together We Thrive</p>
+            <h1 class="hero-title-1">Need any help?</h1>
+            <h2 class="hero-title-2">We got you!</h2>
+            <p class="hero-description">
+                Join our verified local help exchange platform that connects everyone each other. 
+                Discover the joy of giving and receiving support in your community.
+            </p>
+
+            <!-- stats to be dynamic from the database -->
+             <div class="hero-stats">
+                <div class="stat-item">
+                    <div></div>
+                </div>
+             </div>
+        </div>
+    </section>
 </body>
 </html>
