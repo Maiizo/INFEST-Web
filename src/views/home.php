a3535454b75a75cb7a722f229ea292e9e6d46729
felicia-sw <felicia.sword@gmail.com>
2025-05-24 22:40:47 +0700

    updet

--- src/views/home.php
+++ src/views/home.php
@@ -5,24 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+
     <!-- <link rel="stylesheet" href="../../dist/css/style.css"> -->
-     <link rel="stylesheet" href="/INFEST2025/INFEST-Web/dist/css/output.css">
+    <link rel="stylesheet" href="/INFEST2025/INFEST-Web/dist/css/output.css">
 
 </head>
 
 <body>
- <?php include '../components/navbar.html'; ?>
- <?php include '../components/home/hero.html'; ?>    
- <?php include '../components/home/section2.html'; ?>
- <?php include '../components/home/section3.html'; ?>
- <?php include '../components/home/review.html'; ?>
-  <?php include '../components/footer.html'; ?>
-  <?php include '../components/home/BUTTONS.html'; ?>
-  <?php include '../components/signup.html'; ?>
-  
+    <?php include '../components/navbar.html'; ?>
+    <?php include '../components/home/hero.html'; ?>
+    <?php include '../components/home/section2.html'; ?>
+    <?php include '../components/home/section3.html'; ?>
+    <?php include '../components/home/review.html'; ?>
+    <?php include '../components/footer.html'; ?>
+    <?php include '../components/home/BUTTONS.html'; ?>
+    <?php include '../components/signup.html'; ?>
 
- 
+
+
 </body>
 
 </html>
