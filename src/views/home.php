8b9ddd1c7c76ccf8658e981c312f5281d5f7d37f
Eileen <ecynthia01@student.ciputra.ac.id>
2025-05-28 01:45:07 +0700

    fix some routing

--- src/views/home.php
+++ src/views/home.php
@@ -6,12 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <!-- <link rel="stylesheet" href="../../dist/css/style.css"> -->
-    <!-- <link rel="stylesheet" href="../../dist/css/style.css"> -->
-    <!-- In <head> of your HTML or PHP file -->
-    <!-- <link rel="stylesheet" href="../style/style.css"> -->
-   <link rel="stylesheet" href="/INFEST2025/INFEST-Web/src/style/style.css">
- 
+ <!-- <link rel="stylesheet" href="/INFEST2025/INFEST-Web/src/style/style.css"> -->
+ <link rel="stylesheet" href="../style/style.css">
 </head>
 
 <body>
@@ -22,9 +18,6 @@
     <?php include '../components/home/review.html'; ?>
     <?php include '../components/footer.html'; ?>
     <?php include '../components/home/BUTTONS.html'; ?>
-    <?php include '../components/SignUp.html'; ?>
-    <?php include '../components/ShoppingCart.html'; ?>
-    <?php include '../views/BrowseOffers.html'; ?>
 
 </body>
 
